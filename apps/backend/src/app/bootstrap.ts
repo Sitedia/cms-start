7e0dbfaa83709fe87f60718283c4f22a54c0e86b
cedric <cedric@localhost>
2024-05-09 18:34:01 +0200

    eslint

--- apps/backend/src/app/bootstrap.ts
+++ apps/backend/src/app/bootstrap.ts
@@ -3,6 +3,5 @@ import { AppModule } from "./app.module.js";
 
 export const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-
   await app.listen(3000);
 };
---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import eslintPlugin from "@eslint/js";
+import typescriptEslint from "typescript-eslint";
+
+export default [
+  { ignores: ["node_modules", "dist"] },
+  { plugins: { "@typescript-eslint": typescriptEslint.plugin } },
+
+  // Parser for Typescript files
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: typescriptEslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+
+  // Configurations
+  eslintPlugin.configs.recommended,
+  ...typescriptEslint.configs.strictTypeChecked.map((config) => ({
+    files: ["**/*.ts"],
+    ...config,
+  })),
+
+  // Custom configurations for Typescript files
+  {
+    files: ["**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-extraneous-class": ["error", { allowEmpty: true }],
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        { allowNumber: true },
+      ],
+    },
+  },
+];
