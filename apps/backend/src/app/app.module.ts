bd60a0e590d3bd141897956cdc2e0e5cad0cc6a2
cedric <cedric@localhost>
2024-05-09 18:57:58 +0200

    lib

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from "#libs/common";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { options } from "./options.js";
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [options], isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ load: [options], isGlobal: true }),
+    CommonModule,
+  ],
 })
 export class AppModule {}
